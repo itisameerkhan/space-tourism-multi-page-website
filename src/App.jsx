941a21075077849e2c546e25f52f8cf57be15408
itisameerkhan <itisameerkhan@gmail.com>
2023-12-22 08:55:26 +0530

    22/12/2023 08:55

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home/Home';
 import './assets/home/background-home-desktop.jpg';
 import Destination from './Destination/Destination';
+import Crew from './Crew/Crew';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/destination' element={<Destination />} />
+            <Route path='/crew' element={<Crew />} />
           </Routes>
       </div>
     </BrowserRouter>
---
+++ src/Crew/Crew.jsx
@@ -0,0 +1,43 @@
+import './Crew.css';
+import '../assets/destination/background-destination-desktop.jpg';
+import Data from '../data.json';
+import { useState } from 'react';
+
+const Crew = () => {
+
+    const [crewIndex, setCrewIndex] = useState(0);
+
+    return (
+        <div className="crew">
+            <div className="crew-left">
+                <p className='crew-heading'><span>02</span>MEET YOUR CREW</p>
+                <p className="crew-role">{Data?.crew[crewIndex]?.role}</p>
+                <p className="crew-name">{Data?.crew[crewIndex]?.name}</p>
+                <p className="crew-bio">{Data?.crew[crewIndex]?.bio}</p>
+                <div className="crew-navigation">
+                    <div 
+                        className={`crew-nav ${crewIndex === 0 ? 'crew-nav-active' : 'none'}`}
+                        onClick={() => setCrewIndex(0)}
+                    ></div>
+                    <div 
+                        className={`crew-nav ${crewIndex === 1 ? 'crew-nav-active' : 'none'}`}
+                        onClick={() => setCrewIndex(1)}
+                    ></div>
+                    <div 
+                        className={`crew-nav ${crewIndex === 2 ? 'crew-nav-active' : 'none'}`}
+                        onClick={() => setCrewIndex(2)}
+                    ></div>
+                    <div 
+                        className={`crew-nav ${crewIndex === 3 ? 'crew-nav-active' : 'none'}`}
+                        onClick={() => setCrewIndex(3)}
+                    ></div>
+                </div>
+            </div>
+            <div className="crew-right">
+                <img src={Data?.crew[crewIndex]?.images?.png} alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Crew;
